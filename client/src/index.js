b5af8ac0661fab0a516e1bfb84214d3377549cf7
Shreeyasha Pandey <60794230+shree131@users.noreply.github.com>
2024-01-18 18:56:45 -0600

    Made changes to deploy to production

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import './css/style.css';
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
-import '@fortawesome/fontawesome-free/css/all.css';
 
 const modal = new Modal();
 
